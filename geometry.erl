9c958ea26e431b8fb80573677ea751f5cc91779a
Ionel C Gog <gogionel@gmail.com>
2010-10-24 15:05:12 +0100

    Created a basic mapreduce that runs on a single multicore machine.

--- geometry.erl
+++
@@ -1,5 +0,0 @@
--module(geometry).
--export([area/1]).
-area({rectangle, Width, Height}) -> Width * Height;
-area({circle, Radius})           -> 3.14159265 * Radius * Radius;
-area({square, Size})             -> Size * Size.
