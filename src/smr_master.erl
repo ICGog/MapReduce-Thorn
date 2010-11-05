6dbe7b1cd43878ed245e85fecc460c6949d117c4
Vlad Alexandru Ionescu <aether@aether-desktop.(none)>
2010-11-05 14:46:19 +0000

    some worker restart strategy cleanup

--- src/smr_master.erl
+++ src/smr_master.erl
@@ -9,7 +9,8 @@
 -export([init/1, handle_cast/2, handle_call/3, handle_info/2, terminate/2, 
         code_change/3]).
 
--record(state, {workers = dict:new(), jobs = dict:new(), 
+-record(state, {workers = dict:new(),
+                jobs = dict:new(),
                 workers_pid = dict:new()}).
 
 -record(worker, {node,
@@ -61,30 +62,20 @@ init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
-handle_call({spawn_worker, WorkerNode}, _From,
-            State = #state{workers = Workers, workers_pid = WorkersPid}) ->
-    case dict:find(WorkerNode, Workers) of
-        {ok, _Val} ->
-             {reply, {error, already_registered}, State};
-        error ->
-             {ok, WorkerPid} = smr_worker:start_link(WorkerNode),
-             NewWorkers = dict:store(WorkerNode, #worker{node = WorkerNode,
-                                                         pid = WorkerPid},
-                                     Workers),
-             {reply, ok, State#state{workers = NewWorkers,
-                                     workers_pid = dict:store(WorkerPid,
-                                                              WorkerNode,
-                                                              WorkersPid)}}
+handle_call({spawn_worker, Node}, _From, State = #state{workers = Workers}) ->
+    case dict:find(Node, Workers) of
+        {ok, _Val} -> {reply, {error, already_registered}, State};
+        error      -> {reply, ok, restart_worker(Node, State)}
     end;
 
 handle_call({shutdown_worker, WorkerNode}, _From,
             State = #state{workers = Workers, workers_pid = WorkersPid}) ->
     case dict:find(WorkerNode, Workers) of
-        {ok, Val} -> {reply, ok,
-                      State#state{workers = dict:erase(WorkerNode, Workers),
-                                  workers_pid = dict:erase(Val#worker.pid, 
-                                                           WorkersPid)}};
-        error      -> {reply, {error, not_registered}, State}
+        {ok, #worker{pid = Pid}} ->
+            {reply, ok, State#state{workers = dict:erase(WorkerNode, Workers),
+                                    workers_pid = dict:erase(Pid, WorkersPid)}};
+        error ->
+            {reply, {error, not_registered}, State}
     end;
 
 handle_call(get_worker_nodes, _From, State) ->
@@ -112,30 +103,18 @@ handle_cast({job_result, JobPid, Result}, State = #state{jobs = Jobs}) ->
     gen_server:reply((dict:fetch(JobPid, Jobs))#job.from, Result),
     {noreply, State}.
 
-handle_info({'EXIT', JobPid, normal}, State = #state{jobs = Jobs}) ->
-    {noreply, State#state{jobs = dict:erase(JobPid, Jobs)}};
-
-% Catches a worker's exit and tries to restart it using exponential backoff.
-% Carefull, this catches all the EXIT messages.
-handle_info({'EXIT', WorkerPid, _Reason},
-            State = #state{workers = Workers, workers_pid = WorkersPid}) ->
-    NewWorkersPid = dict:erase(WorkerPid, WorkersPid),
-    erlang:send_after(?MIN_BACKOFF_MS, self(), 
-                      {restart_worker, dict:fetch(
-                                            dict:fetch(WorkerPid, WorkersPid), 
-                                            Workers), 
-                       ?MIN_BACKOFF_MS}),
-    {noreply, State#state{workers_pid = NewWorkersPid}};
-
-handle_info({restart_worker, Worker, Backoff},
+handle_info({restart_worker, Node, Backoff},
             State = #state{workers = Workers}) ->
     case Backoff =< ?MAX_BACKOFF_MS of
-        true  -> {noreply, restart_worker(
-                    smr_worker:start_link(Worker#worker.node), 
-                    Worker, Backoff, State)};
-        false -> {noreply, State#state{workers = dict:erase(Worker#worker.node, 
-                                                       Workers)}}
-                  % TODO: Update the statistics
+        true  -> {noreply, restart_worker(Node, 2 * Backoff, State)};
+        false -> {noreply, State#state{workers = dict:erase(Node, Workers)}}
+                 % TODO: Update the statistics
+    end;
+
+handle_info({'EXIT', Pid, Reason}, State = #state{workers_pid = WorkersPid}) ->
+    case dict:find(Pid, WorkersPid) of
+        {ok, WorkerNode} -> handle_worker_exit(Pid, WorkerNode, Reason, State);
+        error            -> handle_other_exit(Pid, Reason, State)
     end;
     
 handle_info(Msg, State) ->
@@ -151,16 +130,33 @@ terminate(_Reason, _State) ->
 % Internal
 %------------------------------------------------------------------------------
 
-restart_worker({ok, WorkerPid}, Worker, _Backoff,
-        State = #state{workers = Workers, workers_pid = WorkersPid}) ->
-    State#state{workers = dict:store(Worker#worker.node, 
-                                     Worker#worker{pid = WorkerPid},
-                                     Workers), 
-                workers_pid = dict:store(WorkerPid, Worker#worker.node,
-                                         WorkersPid)};
+handle_worker_exit(Pid, Node, _Reason,
+                   State = #state{workers_pid = WorkersPid}) ->
+    {noreply,
+     restart_worker(Node,
+                    State#state{workers_pid = dict:erase(Pid, WorkersPid)})}.
 
-restart_worker({error, _Reason}, Worker, Backoff, State) ->
-    NewBackoff = Backoff * 2,
-    erlang:send_after(NewBackoff, self(), {restart_worker, Worker, NewBackoff}),
-    State.
+% TODO: We are assuming job pid
+handle_other_exit(Pid, normal, State = #state{jobs = Jobs}) ->
+    {noreply, State#state{jobs = dict:erase(Pid, Jobs)}}.
 
+restart_worker(Node, State) ->
+    restart_worker(Node, ?MIN_BACKOFF_MS, State).
+
+restart_worker(Node, BackOff, State = #state{workers = Workers,
+                                             workers_pid = WorkersPid}) ->
+    %% TODO log me
+    case smr_worker:start_link(Node) of
+        {ok, Pid} ->
+            Worker = case dict:find(Node, Workers) of
+                         {ok, W} -> W;
+                         error   -> #worker{pid = Pid}
+                     end,
+            State#state{workers = dict:store(Node, Worker#worker{node = Node},
+                                             Workers),
+                        workers_pid = dict:store(Pid, Node, WorkersPid)};
+        _Error ->
+            %% TODO log me
+            erlang:send_after(BackOff, self(), {restart_worker, Node, BackOff}),
+            State
+    end.
