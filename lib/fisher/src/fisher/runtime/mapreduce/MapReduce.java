8c64abb3db7817c5014d5bf461dd569eb60dff2e
Gheorghe-Teodor Bercea <gb308@fusion05.doc.ic.ac.uk>
2010-12-02 14:46:14 +0000

    Thorn to

--- lib/fisher/src/fisher/runtime/mapreduce/MapReduce.java
+++ lib/fisher/src/fisher/runtime/mapreduce/MapReduce.java
@@ -4,8 +4,12 @@ import com.ericsson.otp.erlang.OtpSelf;
 import com.ericsson.otp.erlang.*;
 
 import fisher.runtime.Thing;
+import fisher.runtime.StringTh;
+import fisher.runtime.ListTh;
 import fisher.runtime.IntTh;
+import fisher.runtime.BytesTh;
 import fisher.util.FisherException;
+import java.util.*;
 
 /**
  * This is experimental code. Please implement this properly.
@@ -16,31 +20,175 @@ import fisher.util.FisherException;
 
 public class MapReduce { 
 
-    static String copyright() {
-        return fisher.util.Copyright.IBM_COPYRIGHT;
-    }
-    
-    public static Thing doubleNum(Thing a) {
-        try {
-            OtpSelf self = new OtpSelf("thorn", "authCookie");
-            // The name of the node with which we're communicating.
-            // TODO: Again if possible this should not be hardcoded.
-            OtpPeer other = new OtpPeer("erlide");
-            OtpConnection connection = self.connect(other);
-            
-            OtpErlangObject[] msg = new OtpErlangObject[1];
-			msg[0] = new OtpErlangInt(10);
-			OtpErlangList l = new OtpErlangList(msg);
+    	static String copyright() {
+        	return fisher.util.Copyright.IBM_COPYRIGHT;
+    	}
 
-            // The Name of the module we're communicating with.
-			connection.sendRPC("integration", "double", l);
-			OtpErlangObject received = connection.receiveRPC();
-			System.out.println(received);
-        }
-        catch (Exception e) {
-            // TODO: Catch the right exceptions and handle them accordingly.
-        }
-        return a;
-    }
-    
+	public static Thing doubleNum(Thing a) {
+		return a;
+	}
+
+	public static Thing sendBytesToErlang(Thing m, Thing r, Thing kvp, Thing cookie, Thing peer){
+	  try {
+		//Setup the communication with the erlang node
+		OtpSelf self = new OtpSelf("thorn", cookie.toString());
+            	// The name of the node with which we're communicating.
+            	OtpPeer other = new OtpPeer(peer.toString());
+            	OtpConnection connection = self.connect(other);
+		
+		//create the list of args of the erlang function we're calling
+		OtpErlangObject[] argErlObj = new OtpErlangObject[3];
+		argErlObj[0] = new OtpErlangString(m.toString()); //add the map function
+		argErlObj[1] = new OtpErlangString(r.toString()); //add the reduce function
+
+		//create the list of key-value pairs from the list passed as argument from Thorn
+		ListTh kvpList = (ListTh) kvp;
+		int len = kvpList.length; 
+		OtpErlangObject[] kvpErlObj = new OtpErlangObject[len];
+		int j = 0;
+		byte[] key;
+		byte[] val;
+            	for (Thing elemThing : kvpList.toJavaArray()) {
+                	ListTh elem = (ListTh) elemThing;
+                	key = (byte[]) elem.car().unthingify();
+			val = (byte[]) elem.cdr().car().unthingify();
+			OtpErlangObject[] tuple = new OtpErlangObject[2];
+			tuple[0] = new OtpErlangBinary(key);
+			tuple[1] = new OtpErlangBinary(val);
+			OtpErlangTuple t = new OtpErlangTuple(tuple);
+			kvpErlObj[j] = t;
+			j++;
+            	}
+		OtpErlangList argErlList = new OtpErlangList(kvpErlObj);
+
+		argErlObj[2] = argErlList; //add the list of kvp pairs
+		OtpErlangList args = new OtpErlangList(argErlObj);
+
+		// The Name of the module we're communicating with.
+		// The list of args is of the form: [ map, reduce, [ (key, value), ... ] ]
+	   	connection.sendRPC("integration", "getBytesFromThorn", args); //change the names of the function to match the erlang one
+	    	OtpErlangObject received = connection.receiveRPC();
+		System.out.println(received);
+		connection.close();
+	  } catch (Exception e) {
+	  }
+	  return m;
+	}
+
+	public static Thing getBytesFromErlang(Thing list, Thing cookie, Thing peer){
+	   OtpErlangObject received = null;
+	   ListTh res = (ListTh) list;
+	   try {
+		//Setup the communication with the erlang node
+		OtpSelf self = new OtpSelf("thorn", cookie.toString());
+            	// The name of the node with which we're communicating.
+            	OtpPeer other = new OtpPeer(peer.toString());
+            	OtpConnection connection = self.connect(other);
+
+		OtpErlangObject[] msg = new OtpErlangObject[1];
+		msg[0] = new OtpErlangString("getBytes");
+		OtpErlangList l = new OtpErlangList(msg);
+
+            	// The Name of the module we're communicating with.
+	    	connection.sendRPC("integration", "sendBytesToThorn", l);
+	    	received = connection.receiveRPC();
+
+		//assume that I get back from erlang somethign like this: [ ( bv1, bv2 ), ... ]
+
+		OtpErlangList liist = (OtpErlangList) received;
+                int lenn = liist.elements().length;
+		BytesTh key;
+		BytesTh val;
+                for(int i=0; i<lenn; i++){
+                        key = new BytesTh(((OtpErlangBinary)((OtpErlangTuple)liist.elementAt(i)).elementAt(0)).binaryValue());//.toString().getBytes()); //not sure about this
+                        val = new BytesTh(((OtpErlangBinary)((OtpErlangTuple)liist.elementAt(i)).elementAt(1)).binaryValue());//.toString().getBytes()); //not sure about this
+                        //add the key val to the list
+			res.cons(key);
+			res.cons(val);
+                        //the list should now be of the form [ val, key, val, key, ... ]
+                }
+		//reverse the list to get elements in the right order
+		//in thorn we must then transform the list to [[key, val], ... ]
+		res = res.reversed();
+		connection.close();
+	   } catch(Exception e) {
+	 	
+	   }
+	   return res;
+	}
+
+	public static Thing sendStringToErlang(Thing m, Thing r, Thing kvp, Thing length, Thing cookie, Thing peer){
+	   try {
+		//Setup the communication with the erlang node
+		OtpSelf self = new OtpSelf("thorn", cookie.toString());
+            	// The name of the node with which we're communicating.
+            	OtpPeer other = new OtpPeer(peer.toString());
+            	OtpConnection connection = self.connect(other);
+
+		//create the list of args of the erlang function we're calling
+		OtpErlangObject[] argErlObj = new OtpErlangObject[3];
+		argErlObj[0] = new OtpErlangString(m.toString()); //add the map function
+		argErlObj[1] = new OtpErlangString(r.toString()); //add the reduce function
+
+		//assume we have a list of 2-lists given as a string
+		//[ [k,v], ... ]
+		String list = kvp.toString();
+		//I need the length of the list of values
+		int len = Integer.parseInt(length.toString());
+		StringTokenizer st = new StringTokenizer(list, " ,[]\n\t");
+		OtpErlangObject[] kvpErlObj = new OtpErlangObject[len];
+		int j = 0;
+		String k;
+		String v;
+		while(st.hasMoreTokens()){
+			k = st.nextToken();
+			v = st.nextToken();
+			OtpErlangObject[] tuple = new OtpErlangObject[2];
+			tuple[0] = new OtpErlangString(k);
+			tuple[1] = new OtpErlangString(v);
+			OtpErlangTuple t = new OtpErlangTuple(tuple);
+			kvpErlObj[j] = t;
+			j++;
+		}
+		OtpErlangList argErlList = new OtpErlangList(kvpErlObj);
+
+		argErlObj[2] = argErlList; //add the list of kvp pairs
+		OtpErlangList args = new OtpErlangList(argErlObj);
+
+		// The Name of the module we're communicating with.
+	   	connection.sendRPC("integration", "getStringsFromThorn", args); //change the names of the function to match the erlang one
+	    	OtpErlangObject received = connection.receiveRPC();
+		System.out.println(received);
+		connection.close();
+	  } catch (Exception e) {
+	  }
+	  return m;
+	}
+
+	public static Thing getStringFromErlang(Thing cookie, Thing peer){
+           OtpErlangObject received = null;
+	   try {
+		//Setup the communication with the erlang node
+		OtpSelf self = new OtpSelf("thorn", cookie.toString());
+            	// The name of the node with which we're communicating.
+            	OtpPeer other = new OtpPeer(peer.toString());
+            	OtpConnection connection = self.connect(other);
+
+		OtpErlangObject[] msg = new OtpErlangObject[1];
+		msg[0] = new OtpErlangString("getString");
+		OtpErlangList l = new OtpErlangList(msg);
+
+            	// The Name of the module we're communicating with.
+	    	connection.sendRPC("integration", "sendStringsToThorn", l);
+	    	received = connection.receiveRPC();
+	    	//System.out.println(received);
+		connection.close();
+	   } catch(Exception e) {
+	 	System.out.println(e);
+	   }
+	   if (received == null){
+		return StringTh.of("Error in getting a string from Erlang");
+	   }
+	   return StringTh.of(received.toString());
+	}    
 }
--- src/thorn/integration.erl
+++ src/thorn/integration.erl
@@ -1,5 +1,6 @@
 -module(integration).
 
+-export([getBytesFromThorn/3, sendBytesToThorn/1, getStringsFromThorn/3, sendStringsToThorn/1]).
 -export([double/1, add_values/0, start/0]).
 
 add_values() -> 
@@ -12,3 +13,13 @@ start() ->
  register(add_test, spawn(test, add_values, [])).
 
 double(X) -> 2 * X.
+
+getBytesFromThorn(Map,Reduce,KvpList) -> io:format("~p~n~p~n~p~n", [Map,Reduce,KvpList]).
+
+sendBytesToThorn(_X) -> [].
+
+getStringsFromThorn(Map,Reduce,KvpList) -> io:format("~p~n~p~n~p~n", [Map,Reduce,KvpList]).
+
+sendStringsToThorn(_X) -> [{"A","10"}, {"B","90"}, {"C","100"}].
+
+
