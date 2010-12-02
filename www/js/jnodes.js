49a85b43768ba7cfd35837bc393fadb764609b10
Chia Yao <ccy08@vector44.doc.ic.ac.uk>
2010-12-02 11:27:19 +0000

    jnodes.html and jnodes.js now works with smr:get_workers

--- www/js/jnodes.js
+++ www/js/jnodes.js
@@ -4,7 +4,7 @@
 
 function getNodes() {
 
-$.get("testworkers.js",
+$.get("http://localhost:8081/smr/smr_http:get_workers",
   function(data){
     var number = data.length;
     var current;
@@ -12,15 +12,13 @@ $.get("testworkers.js",
     for(current = 0; current<number; current++) {
       var temp = data[current];
       var worker = new Object();
-      worker.name = temp.name;
-      worker.starton = temp.started_on;
+      worker.ndoe = temp.node;
       worker.exec = temp.num_exec;
       worker.succ = temp.num_succ;
       worker.fail = temp.num_failed;
       worker.busy = temp.busy_time;
-      worker.map = temp.num_map_jobs;
-      worker.red = temp.num_reduce_jobs;
-      worker.starttime = temp.start_time;
+      worker.map = temp.num_map_tasks;
+      worker.red = temp.num_reduce_tasks;
       result[current] = worker;
     }
     return result;
