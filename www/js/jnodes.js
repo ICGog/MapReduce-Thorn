858fb7432d44ebfb29aee3a469e9a9a86a06032d
Shelley Yao <shelley@shelley-laptop.(none)>
2010-12-05 11:34:15 +0000

    jnodes.js now works with jnodes.html

--- www/js/jnodes.js
+++ www/js/jnodes.js
@@ -3,12 +3,15 @@
 // url would be changed once know specific location
 
 function getNodes() {
-
-$.get("http://localhost:8081/smr/smr_http:get_workers",
-  function(data){
+var aResult = new Array();
+$.ajax({
+  url: "http://localhost:8081/smr/smr_http:get_workers",
+  dataType:'json',
+  async:false,
+  success:
+  function (data){
     var number = data.length;
     var current;
-    var result = new Array();
     for(current = 0; current<number; current++) {
       var temp = data[current];
       var worker = new Object();
@@ -19,8 +22,9 @@ $.get("http://localhost:8081/smr/smr_http:get_workers",
       worker.busy = temp.busy_time;
       worker.map = temp.num_map_tasks;
       worker.red = temp.num_reduce_tasks;
-      result[current] = worker;
+      aResult[current] = worker;
     }
-    return result;
-});
+}});
+return aResult;
+};
 
